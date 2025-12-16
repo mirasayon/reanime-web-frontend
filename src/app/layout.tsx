49d72b7366f206fd9bc7c6ee5ae2af2cab84e5d9
Miralisher <mirasayon@ya.ru>
2025-12-17 03:18:35 +0500

    updated dependencies, including `next` that solves `medium` vulnerability. Added first `proxy.ts` (middleware) to log request realtime.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,24 @@
-import "#/styles/global/main.tailwind.css";
-import type { Metadata } from "next";
-import { Cookie_consent_banner } from "#/components/layout/cookie_consent-button";
+import { Google_Analytics, Google_TagManager } from "#/components/analytics/google-analytics";
 import { YandexMetrikaAnalytics } from "#/components/analytics/yandex_metrika";
-import { inter } from "#/fonts/main-font.provider";
-import type { LayoutProps } from "#T/nextjs";
-import layoutStyles from "#/styles/global/layout.module.css";
-import {
-    Google_Analytics,
-    Google_TagManager,
-} from "#/components/analytics/google-analytics";
-import { ThemeProviderCustom } from "#/components/themes/provider.themes";
+import { JotaiMainProvider } from "#/components/layout/atoms-toasts-components/jotai-main-provider";
+import { Cookie_consent_banner } from "#/components/layout/cookie_consent-button";
 import { Layout_Footer } from "#/components/layout/global/global-main-footer";
 import { GlobalMainHeader } from "#/components/layout/global/global-main-header";
-import { root_layout_Metadata } from "#/meta/root-layout.metadata";
+import { ThemeProviderCustom } from "#/components/themes/provider.themes";
+import { inter } from "#/fonts/main-font.provider";
 import { HtmlElementForJsonLD } from "#/meta/json_ld.static-metadata-setter";
+import { root_layout_Metadata } from "#/meta/root-layout.metadata";
+import layoutStyles from "#/styles/global/layout.module.css";
+import "#/styles/global/main.tailwind.css";
+import type { LayoutProps } from "#T/nextjs";
+import type { Metadata } from "next";
 import type { JSX } from "react";
-import { JotaiMainProvider } from "#/components/layout/atoms-toasts-components/jotai-main-provider";
 const isAnalyticsEnabled = process.env.NEXT_PUBLIC_ANALYTICS === "true";
 const gAIdPublic = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!;
 const gTMidPublic = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID!;
 const userServiceUrlPublic = process.env.NEXT_PUBLIC_USER_SERVICE_URL!;
 type __Root_layoutProps = LayoutProps;
-export default function __Root_layout({
-    children,
-}: __Root_layoutProps): JSX.Element {
+export default function __Root_layout({ children }: __Root_layoutProps): JSX.Element {
     return (
         <html lang="ru" suppressHydrationWarning>
             <head>
@@ -33,9 +28,7 @@ export default function __Root_layout({
             <body className={`${inter.className} ${layoutStyles.rootWeb}   `}>
                 <ThemeProviderCustom>
                     <JotaiMainProvider>
-                        <GlobalMainHeader
-                            userServiceBaseUrl={userServiceUrlPublic}
-                        />
+                        <GlobalMainHeader userServiceBaseUrl={userServiceUrlPublic} />
                         {children}
                         <Layout_Footer />
                         <Cookie_consent_banner />
