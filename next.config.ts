270704d550747c8bc6e516773fd855fdef158901
Miralisher <mirasayon@ya.ru>
2026-01-05 01:01:27 +0500

    chore(config): update next config and refactor postcss config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,6 @@ export default {
             bodySizeLimit: "20mb",
         },
     },
-    allowedDevOrigins: ["192.168.0.105"],
+    allowedDevOrigins: [],
     poweredByHeader: false,
 } satisfies NextConfig;
