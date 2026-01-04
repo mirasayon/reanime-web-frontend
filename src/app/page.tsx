a2abb0b4aba8625a000a9e56c1134e9ea687c1a0
Miralisher <mirasayon@ya.ru>
2026-01-04 21:45:58 +0500

    refactor(root-page): clean up unused and commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,23 @@
 "use server";
-import type { SearchParams } from "#T/nextjs";
 import { AnimeMainPageCarousel } from "#/components/anime-carousel-main-page/anime-carousel-show";
 import { Anime_List_Component } from "#/components/utilities/common/assembler-of-utilities.utility-components";
 import { kodikClient } from "#/providers/kodik-api";
 import { topChartAnimesStaticData } from "#/constants/anime-genres/top-chart-animes.static";
 import { dedupeAnimes } from "#/utils/reducer-deduper";
 
-export default async function __Home_RootPage({ searchParams }: { searchParams: SearchParams }) {
-    // const auth = await getSessionFromClient({ cookies: await cookies(), headers: await headers() });
-    const searchPms = await searchParams;
-    const kodikResponse = await kodikClient.list({
+export default async function __Home_RootPage() {
+    const { results } = await kodikClient.list({
         with_material_data: true,
-        limit: Number(searchPms.limit) || 100,
+        limit: 100,
         has_field: "shikimori_id",
         order: "desc",
         sort: "shikimori_rating",
         types: ["anime", "anime-serial"],
     });
-    const { results: data } = kodikResponse;
     return (
         <>
-            {/* <Welcome_for_home_page logged={!!auth} /> */}
             <AnimeMainPageCarousel animes={topChartAnimesStaticData} />
-            <Anime_List_Component data={dedupeAnimes(data)} />
+            <Anime_List_Component data={dedupeAnimes(results)} />
         </>
     );
 }
