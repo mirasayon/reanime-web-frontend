d8d70f80b62eefe493bb8c63e2d8a80c8773dd25
Mirasayon <mirasayon@ya.ru>
2026-01-27 17:44:07 +0500

    feat(metadata): add page title for user page

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -14,20 +14,22 @@ import type {
 } from "#user-service/user-service-response-types-for-all.routes.ts";
 import { endpointsConfig } from "#user-service/endpoints-config.ts";
 import { AnimeBookmarkCollectionForProfile } from "../anime-collections-for-profile/anime-bookmark-collection-for-profile";
-export default async function __User__Page({
+import type { Metadata } from "next";
+export default async function __UserPage({
     searchParams,
     params,
 }: {
     params: Promise<{ username: string }>;
-
     searchParams: Promise<{ animeStatusTab: string | undefined }>;
 }): Promise<React.JSX.Element> {
     const _searchParams = await searchParams;
     const _params = await params;
-    const _username = _params.username;
+    if (_params.username?.length < 4) {
+        return notFound();
+    }
     const auth = await getAccountSession();
     const base_profile_data = await userServiceRequest<ResponseTypesFor_UserProfile_Section["view_other_profiles"]>(
-        endpointsConfig.userProfile.baseUrl + endpointsConfig.userProfile.exploreOthersProfile(_username),
+        endpointsConfig.userProfile.baseUrl + endpointsConfig.userProfile.exploreOthersProfile(_params.username),
         "GET",
     );
     if (!base_profile_data || !base_profile_data.ok || !base_profile_data?.data) {
@@ -37,7 +39,7 @@ export default async function __User__Page({
         ResponseTypesFor_CommentForAnime_Section["all_for_public_profile"]
     >(
         endpointsConfig.commentAboutAnime.baseUrl +
-            endpointsConfig.commentAboutAnime.getAllCommentsFromProfile(_username) +
+            endpointsConfig.commentAboutAnime.getAllCommentsFromProfile(_params.username) +
             "?page=1&limit=100",
         "GET",
     );
@@ -51,24 +53,21 @@ export default async function __User__Page({
     if (!my_profile_data || my_profile_data.status_code === 404 || !my_profile_data.data) {
         return notFound();
     }
-    const loggedUser = my_profile_data.data;
-
     const animeCollection = await userServiceRequest<ResponseTypesFor_AnimeBookmark_Section["get_all_list"]>(
         endpointsConfig.animeBookmarks.baseUrl + endpointsConfig.animeBookmarks.getAllList,
         "GET",
     );
-    const collectionData = animeCollection.data;
     const watchingIds =
-        collectionData?.filter((item) => item.status === "WATCHING").map((item) => item.external_anime_id) || [];
+        animeCollection.data?.filter((item) => item.status === "WATCHING").map((item) => item.external_anime_id) || [];
     const abandonedIds =
-        collectionData?.filter((item) => item.status === "ABANDONED").map((item) => item.external_anime_id) || [];
+        animeCollection.data?.filter((item) => item.status === "ABANDONED").map((item) => item.external_anime_id) || [];
     const plannedIds =
-        collectionData?.filter((item) => item.status === "PLANNED").map((item) => item.external_anime_id) || [];
+        animeCollection.data?.filter((item) => item.status === "PLANNED").map((item) => item.external_anime_id) || [];
     const completedIds =
-        collectionData?.filter((item) => item.status === "COMPLETED").map((item) => item.external_anime_id) || [];
+        animeCollection.data?.filter((item) => item.status === "COMPLETED").map((item) => item.external_anime_id) || [];
     return (
         <>
-            <MyProfileDashboard data={loggedUser} />
+            <MyProfileDashboard data={my_profile_data.data} />
             <div className={BORDER}>
                 <SecuritySettingsDashboardComponent />
                 {all_comments_from_this_user ? (
@@ -86,4 +85,15 @@ export default async function __User__Page({
             />
         </>
     );
+}
+
+export async function generateMetadata({ params }: { params: Promise<{ username: string }> }): Promise<Metadata> {
+    const _params = await params;
+    if (_params.username.length < 4) {
+        return notFound();
+    }
+    return {
+        title: `@${_params.username}`,
+        description: `Профиль пользователя @${_params.username}`,
+    };
 }
