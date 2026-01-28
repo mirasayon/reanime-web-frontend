edd4039177333141dd0e45509a0e7a14c044b39a
Mirasayon <mirasayon@ya.ru>
2026-01-28 06:10:02 +0500

    fix(styles): fix styles for dropdown menu at header

--- src/components/dropdown-menu-in-head-corner/for-logged-users.tsx
+++ src/components/dropdown-menu-in-head-corner/for-logged-users.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 import { LogoutForHeaderComponent } from "../auth/logout-for-header-component";
 export const DASHBOARD_LINKS_STYLE =
-    "dark:border-white border-black my-1 w-full text-center hover:bg-blue-800/80 rounded cursor-pointer p-1 border-2  ";
+    "dark:border-white flex border-black my-1 w-full text-center hover:bg-blue-800/80 rounded cursor-pointer p-1 border-2  ";
 export const DASHBOARD_LINKS_STYLE_RED_BORDER =
     "border-red-500 dark:border-red-500 hover:bg-red-500 my-1 w-full text-center rounded cursor-pointer p-1 border-2  ";
 export function AvatarDashboardForLoggedInUser({ username }: { username: string }) {
